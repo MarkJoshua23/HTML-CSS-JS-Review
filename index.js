bff973a7a8006514fd1eaf3505c1687bbcdb29a9
MarkJoshua23 <markjoshuatarcelo@gmail.com>
2024-08-18 22:09:33 +0800

    responsive

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var  tablinks = document.getElementsByClassName("tab-links");
 var  tabcontents = document.getElementsByClassName("tab-contents");
+var sidebar = document.getElementById('sidebar')
 
 function opentab(tabname){
     for(i of tablinks){
@@ -11,3 +12,31 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab")
 }
+
+// --------set the right on nav ul of css to 0 which will display the sidebar-----
+function openbar(){
+    sidebar.style.right = '0';
+}
+
+function closebar(){
+    sidebar.style.right = '-200px';
+}
+
+
+// -----form to sheets----
+const scriptURL = 'scripturl'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById('msg')
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        msg.innerHTML = " Message Sent Successfully";
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000);
+        form.reset();
+    })
+    .catch(error => console.error('Error!', error.message))
+})
